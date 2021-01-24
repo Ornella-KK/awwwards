e2d383de54005a5b058ef1c8a60bca8384907c76
Ornella-KK <ornellakankazi@gmail.com>
2021-01-24 21:28:06 +0200

    updated the forms

---
+++ awards/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Profile,Projects,Rates
+
+class ProjectUploadForm(forms.ModelForm):
+    class Meta:
+        model = Projects
+        fields = ('title','image_landing','description', 'link')
+        
+        
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('profile_photo','bio','website') 
+        
+class VotesForm(forms.ModelForm):
+    class Meta:
+        model = Rates
+        fields = ('design','usability','content')
+        
