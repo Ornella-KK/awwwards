bf0b63fe48d1c33c0948f912ea2c0069c43652ad
Ornella-KK <ornellakankazi@gmail.com>
2021-01-25 12:27:11 +0200

    added some installations and solved some errors in different files

--- app/settings.py
+++ app/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'awards',
+        'NAME':'awwards',
         'USER':'ornella',
         'PASSWORD':'kko',  
     }
